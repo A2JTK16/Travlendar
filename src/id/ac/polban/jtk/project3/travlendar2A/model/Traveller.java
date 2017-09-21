7292c13b754207d20b1d228b1f3c6f870cfabdce
Naufal M Hibatullah <naufalmwh11@gmail.com>
2017-09-21 17:37:42 +0700

    Mengembalikan fungsi setter dikarenakan dalam mock up ada page Setting account

--- src/id/ac/polban/jtk/project3/travlendar2A/model/Traveller.java
+++ src/id/ac/polban/jtk/project3/travlendar2A/model/Traveller.java
@@ -19,62 +19,91 @@ public class Traveller {
     private String email;
     private String password;
     private String fullname;
-    
-    Traveller() {
-        
-    }
-    
-    Traveller(String fullname, String username, String email, String password) {
+   
+    public Traveller(String fullname, String username, String email, String password) {
         this.fullname = fullname;
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
+    
+    /*
+     * Setter digunakan untuk setting apabila data Traveller telah di diisi dengan menggunakan konstruktor 
+     * Karena apabila tidak ada setter maka variabel akan/harus menjadi bertipe final(konstan) sehingga
+     * nantinya variabel tersebut tidak dapat di rubah isinya
+    */
+    
     /**
      * @return the id_traveller
      */
-    public int getId_traveller() { //mengambil value id_traveller
+    public int getId_traveller() {
         return id_traveller;
     }
 
     /**
      * @param id_traveller the id_traveller to set
      */
-//mengubah value id_traveller
-    public void setId_traveller(int id_traveller) { 
+    public void setId_traveller(int id_traveller) {
         this.id_traveller = id_traveller;
     }
 
     /**
      * @return the username
      */
-    public String getUsername() { //mengambil value username
+    public String getUsername() {
         return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     /**
      * @return the email
      */
-    public String getEmail() { //mengambil value email
+    public String getEmail() {
         return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     /**
      * @return the password
      */
-    public String getPassword() { //mengambil value password
+    public String getPassword() {
         return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     /**
      * @return the fullname
      */
-    public String getFullname() {//mengambil value fullname
+    public String getFullname() {
         return fullname;
     }
 
-    /*public static void main(String[] args) {
+    /**
+     * @param fullname the fullname to set
+     */
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public static void main(String[] args) {
         Traveller data = new Traveller("maryammm", "maryam@gmail.com", "Siti Maryam", "maryam123");
         data.setId_traveller(421998);
         System.out.println("Id Traveller : " + data.getId_traveller());
@@ -82,5 +111,6 @@ public class Traveller {
         System.out.println("Full Name    : " + data.getFullname());
         System.out.println("Email        : " + data.getEmail());
         System.out.println("Password     : " + data.getPassword());
-    }*/
+    }
+    
 }
