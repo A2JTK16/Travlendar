1eb9c5239713ab80a3b726694acfe2df5615a5a5
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2017-09-26 07:35:38 +0700

    Update Distance Matrix and Location

--- src/id/ac/polban/jtk/project3/travlendar2A/model/DistanceMatrix.java
+++ src/id/ac/polban/jtk/project3/travlendar2A/model/DistanceMatrix.java
@@ -12,27 +12,9 @@ package id.ac.polban.jtk.project3.travlendar2A.model;
 public class DistanceMatrix {
     //deklarasi atribut
     double distance;
-    private String [] kota = {"Bandung", "Bekasi", "Bogor",	"Ciamis", "Cianjur","Cirebon","Garut","Indramayu","Karawang","Kuningan","Majalengka", "Pangandaran","Purwakarta","Subang","Sukabumi","Sumedang","Tasikmalaya"};
-    
-    /**
-     * REVIEW 
-     * 
-     * 1.   Kota_awal dan kota_tujuan Bentuk Integer Saja, Tidak Memerlukan String.
-     *      Selanjutnya, Kode Kota Tersebut Diubah Menjadi Alamat Indeks Array,
-     *      Lalu Kembalikan Isi Array Tersebut.
-     * 2.   Kota Bisa Pakai 'enum' untuk Memudahkan
-     * 3.   Diperlukan Method untuk Mengembalikan Nilai Jarak
-     * 4.   Lebih Baik Teknik Mendapatkan Jarak Pakai Indeks Array Saja Langsung,
-     *      Tidak Memerlukan Nested Loop Karena Kode Kota Tidak Berubah dan
-     *      Memiliki Keterkaitan dengan Indeks Array 
-     *      Contoh : Array jarak[1][2] Berisi Jarak Bandung - Bekasi
-     */
-    
-    //private String kota_awal; //deklarasi variabel kota_awal yang bertipe String
-    //private String kota_tujuan; //deklarasi variabel kota_tujuan yang bertipe String
-    
-    private int [] kode_kota1 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}; //deklarasi variabel kode_kota1 yang bertipe integer
-    private int [] kode_kota2 = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}; //deklarasi variabel kode_kota2 yang bertipe integer
+    private double jarak_lokasi;
+    private int  kode_kota1; 
+    private int kode_kota2; 
     private double [][] jarak = {{0,	134,	177,	135,	160,	209,	149,	201,	89.4,	244,	192,	202,	53.3,	118,	90.1,	71.1,	126},
                                {132,	0,	62.2,	248,	208,	200,	199,	192,	54.2,	236,	183,	316,	78.4,	110,	173,	184,	240},
                                {212,    81.8,   0,      305,	175,    247,    319,	249,	110,	292,	239,	372,	135,	166,	140,	241,	296},
@@ -52,59 +34,36 @@ public class DistanceMatrix {
                                {119,	231,	299,	19.6,	218,	119,	140,	151,	207,	88.8,	92.4,	84.7,	171,	183,	208,	96.8,	0},
                              };
     
-    public String[] getKota() {
-        return kota;
-    }
-
-    //method ini belum diimplementasikan pada program
-    /*public void setKota(String[] kota) { 
-        this.kota = kota;
-    }
-
-    public int[] getKode_kota() { //
-        return kode_kota;
-    }
-    
-    public void setKode_kota(int[] kode_kota) {
-        this.kode_kota = kode_kota;
-    }
-
-    public String getKota_awal() {
-        return kota_awal;
-    }
-
-    public void setKota_awal(String kota_awal) {
-        this.kota_awal = kota_awal;
-    }*/
-   
-    //method untuk mengecek kota
-   public void cek_kota (String kota_awal, String kota_tujuan)
-    {
-        for (int n=0; n<17; n++) //perulangan untuk kota awal
-        {
-            for (int m=0;m<17;m++) //perulangan untuk kota tujuan
-            {
-                if (kota_awal.equals(getKota()[n]) && kota_tujuan.equals(getKota()[m]) ) //pengecekan kota untuk mendapatkan kode kota agar bisa mengetahui jarak
-                {
-                     this.distance= (getJarak()[n][m]); //menampilkan jarak antara kota awal dan kota tujuan
-                } 
-            }
-        }
-    }
-
-    /**
-     * @return the jarak
-     */
     public double[][] getJarak() {
         return jarak;
     }
     public double getdistance(){
         return distance;
     }
-    /**
-     * @param jarak the jarak to set
-     */
+    
     public void setJarak(double[][] jarak) {
         this.jarak = jarak;
+    }
+
+    public void setKode_kota1(int kode_kota1) {
+        this.kode_kota1 = kode_kota1;
+    }
+
+    public int getKode_kota2() {
+        return kode_kota2;
+    }
+
+    public void setKode_kota2(int kode_kota2) {
+        this.kode_kota2 = kode_kota2;
+    }
+
+    //method return jarak
+    public double getJarak_lokasi() {
+        this.jarak_lokasi = getJarak()[kode_kota1][kode_kota2];
+        return jarak_lokasi;
+    }
+
+    public void setJarak_lokasi(int jarak_lokasi) {
+        this.jarak_lokasi = jarak_lokasi;
     }
 }
--- src/id/ac/polban/jtk/project3/travlendar2A/model/Location.java
+++ src/id/ac/polban/jtk/project3/travlendar2A/model/Location.java
@@ -12,83 +12,46 @@ package id.ac.polban.jtk.project3.travlendar2A.model;
  * @author Auliya Aqma Dinillah
  */
 public class Location {
-    //atribute
-    private int kode_lokasi;
-    private String nama_lokasi;
+    private String [] nama_lokasi = {"Bandung", "Bekasi", "Bogor","Ciamis", "Cianjur","Cirebon","Garut","Indramayu","Karawang","Kuningan","Majalengka", "Pangandaran","Purwakarta","Subang","Sukabumi","Sumedang","Tasikmalaya"};
+    private int [] kode_lokasi = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+    private String Nama_Lokasi;
+    private int Kode_Lokasi;
 
-    /**
-     * @return the kode_lokasi
-     */
-    public int getKode_lokasi() {
-        return kode_lokasi;
-    }
-
-    /**
-     * @param kode_lokasi the kode_lokasi to set
-     */
-    public void setKode_lokasi(int kode_lokasi) {
-        this.kode_lokasi = kode_lokasi;
-    }
-
-    /**
-     * @return the nama_lokasi
-     */
-    public String getNama_lokasi() {
+    public String[] getNama_lokasi() {
         return nama_lokasi;
     }
 
-    /**
-     * @param nama_lokasi the nama_lokasi to set
-     */
-    public void setNama_lokasi(String nama_lokasi) {
+    public void setNama_lokasi(String[] nama_lokasi) {
         this.nama_lokasi = nama_lokasi;
     }
-/*    public static void main(String[] args) {
-        Location lok_1 = new Location();
-        lok_1.setKode_lokasi(10001);
-        lok_1.setNama_lokasi("Rumah");
-        
-        Location lok_2 = new Location();
-        lok_2.setKode_lokasi(10002);
-        lok_2.setNama_lokasi("Kantor DPRD Bangka Tengah");
-        
-        Location lok_3 = new Location();
-        lok_3.setKode_lokasi(10003);
-        lok_3.setNama_lokasi("Bandara Depati Amir");
-        
-        Location lok_4 = new Location();
-        lok_4.setKode_lokasi(20001);
-        lok_4.setNama_lokasi("Bandara Soekarno Hatta");
-        
-        Location lok_5 = new Location();
-        lok_5.setKode_lokasi(20002);
-        lok_5.setNama_lokasi("Kantor DPRD Jakarta");
-        
-        Location lok_6 = new Location();
-        lok_6.setKode_lokasi(30001);
-        lok_6.setNama_lokasi("Tangerang");
-        
-        Location lok_7 = new Location();
-        lok_7.setKode_lokasi(20001);
-        lok_7.setNama_lokasi("Bandara Soekarno Hatta");
-        
-        Location lok_8 = new Location();
-        lok_8.setKode_lokasi(10002);
-        lok_8.setNama_lokasi("Kantor DPRD Bangka Tengah");
-        
-        Location lok_9 = new Location();
-        lok_9.setKode_lokasi(10001);
-        lok_9.setNama_lokasi("Rumah");
-        
-        System.out.print(lok_1.getNama_lokasi() + " -> ");
-        System.out.print(lok_2.getNama_lokasi() + " -> ");
-        System.out.print(lok_3.getNama_lokasi() + " -> ");
-        System.out.print(lok_4.getNama_lokasi() + " -> ");
-        System.out.print(lok_5.getNama_lokasi() + " -> ");
-        System.out.print(lok_6.getNama_lokasi() + " -> ");
-        System.out.print(lok_7.getNama_lokasi() + " -> ");
-        System.out.print(lok_8.getNama_lokasi() + " -> ");
-        System.out.print(lok_9.getNama_lokasi());
+
+    public int[] getKode_lokasi() {
+        return kode_lokasi;
     }
-*/
+
+    public void setKode_lokasi(int[] kode_lokasi) {
+        this.kode_lokasi = kode_lokasi;
+    }
+
+    public int getKode_Lokasi() {
+        for (int i=0;i<17;i++)
+        {
+          if (Nama_Lokasi.equals(nama_lokasi[i]))  
+          this.Kode_Lokasi=kode_lokasi[i];
+        }
+        return Kode_Lokasi;
+    }
+
+    public void setKode_Lokasi(int Kode_Lokasi) {
+        this.Kode_Lokasi = Kode_Lokasi;
+    }
+
+    public String getNama_Lokasi() {
+        return Nama_Lokasi;
+    }
+
+    public void setNama_Lokasi(String Nama_Lokasi) {
+        this.Nama_Lokasi = Nama_Lokasi;
+    }
 }
+
