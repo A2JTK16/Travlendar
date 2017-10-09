d86f3599f7aebe3f49a5af2fc51907a86910c182
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2017-10-09 19:49:09 +0700

    penambahan class kota dan provinsi

---
+++ src/id/ac/polban/jtk/project3/travlendar2A/model/Kota.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polban.jtk.project3.travlendar2A.model;
+
+/**
+ *
+ * @author Auliya Aqma Dinillah
+ */
+public class Kota {
+    private int kd_kota;
+    private String nm_kota;
+
+    /**
+     * @return the kd_kota
+     */
+    public int getKd_kota() {
+        return kd_kota;
+    }
+
+    /**
+     * @param kd_kota the kd_kota to set
+     */
+    public void setKd_kota(int kd_kota) {
+        this.kd_kota = kd_kota;
+    }
+
+    /**
+     * @return the nm_kota
+     */
+    public String getNm_kota() {
+        return nm_kota;
+    }
+
+    /**
+     * @param nm_kota the nm_kota to set
+     */
+    public void setNm_kota(String nm_kota) {
+        this.nm_kota = nm_kota;
+    }
+    
+}
