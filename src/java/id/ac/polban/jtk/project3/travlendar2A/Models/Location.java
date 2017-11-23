a96cc6cc578be67f6435ee611d8d4aa0e86174fc
Auliya Aqma Dinillah <auliyaaqma@gmail.com>
2017-11-23 11:06:26 +0700

    menambahkan class location

---
+++ src/java/id/ac/polban/jtk/project3/travlendar2A/Models/Location.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.polban.jtk.project3.travlendar2A.Models;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Location {
+    private Integer location_id;
+    private Float latitude;
+    private Float longitude;
+
+    /**
+     * @return the location_id
+     */
+    public Integer getLocation_id() {
+        return location_id;
+    }
+
+    /**
+     * @param location_id the location_id to set
+     */
+    public void setLocation_id(Integer location_id) {
+        this.location_id = location_id;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @param latitude the latitude to set
+     */
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @param longitude the longitude to set
+     */
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+    
+}
