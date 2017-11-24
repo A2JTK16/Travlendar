57694d5e32850431be4caf0e22f1a63aefe5d8de
Mufid Jamaluddin <mufid.jamaluddin@gmail.com>
2017-11-25 03:21:17 +0700

    Mengubah list menjadi set

--- test/id/ac/polban/jtk/project3/travlendar2A/Test/FunctionalTest/TestHelper/TestShowList.java
+++ test/id/ac/polban/jtk/project3/travlendar2A/Test/FunctionalTest/TestHelper/TestShowList.java
@@ -17,21 +17,33 @@ import java.util.List;
  *
  * @author mufidjamaluddin
  */
-public class TestShowListEvent 
+public class TestShowList 
 {
     public static void main(String[] args)
     {
+        /**
+         * Test menampilkan list Event
+         */
         IDao<Event> dao;
         dao = new GenericDao<>("jdbc:mysql://localhost:3306/a2travlendar","root","",Event.class);
     
         List<Event> list;
         list = dao.getList();
         
+        list.forEach((event) -> {
+            System.out.println(event.getEvent_id() + " | "+ event.getEvent_name() + " | " + event.getStart_event() + " | " + event.getEnd_event() + " | " + event.getNote());
+        });
         
-            list.forEach((event) -> {
-                System.out.println(event.getEvent_id() + " | "+ event.getEvent_name() + " | " + event.getStart_event() + " | " + event.getEnd_event() + " | " + event.getNote());
-            });
+        boolean delete = dao.delete("event_id", "5");
+        if(!delete) System.out.println("Delete gagal");
+        list = dao.getList();
         
+        list.forEach((event) -> {
+            System.out.println(event.getEvent_id() + " | "+ event.getEvent_name() + " | " + event.getStart_event() + " | " + event.getEnd_event() + " | " + event.getNote());
+        });
+        /**
+         * Test menampilkan list traveller
+         */
         IDao<Traveller> daoTraveller;
         daoTraveller = new GenericDao<>("jdbc:mysql://localhost:3306/a2travlendar","root","",Traveller.class);
         
@@ -41,6 +53,9 @@ public class TestShowListEvent
             System.out.println( t.getTraveller_fullname() + " | " + t.getTraveller_email() + " | " + t.getTraveller_password());
         });
         
+        /**
+         * Test menampilkan list location
+         */
         IDao<Location> daoLocation;
         daoLocation = new GenericDao<>("jdbc:mysql://localhost:3306/a2travlendar","root","",Location.class);
         
